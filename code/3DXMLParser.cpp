cba84671720015b7b794114cb75b66485d631158
Léo Terziman <leo.terziman@clarte.asso.fr>
2013-12-13 10:40:08 +0100

    In 3DXML: small code safety improvements

--- code/3DXMLParser.cpp
+++ code/3DXMLParser.cpp
@@ -1128,13 +1128,15 @@ namespace Assimp {
 		parser->ParseElement(mapping, params);
 
 		// Test if the name exist, otherwise use the id as name
-		if(params.name_opt) {
-			params.mesh->name = *(params.name_opt);
-			params.mesh->has_name = true;
-		} else {
-			// No name: take the id as the name
-			params.mesh->name = parser->ToString(params.id);
-			params.mesh->has_name = false;
+		if(params.mesh != nullptr) {
+			if(params.name_opt) {
+				params.mesh->name = *(params.name_opt);
+				params.mesh->has_name = true;
+			} else {
+				// No name: take the id as the name
+				params.mesh->name = parser->ToString(params.id);
+				params.mesh->has_name = false;
+			}
 		}
 	}
 	
@@ -1347,13 +1349,15 @@ namespace Assimp {
 		parser->ParseElement(mapping, params);
 
 		// Test if the name exist, otherwise use the id as name
-		if(params.name_opt) {
-			params.material->name = *(params.name_opt);
-			params.material->has_name = true;
-		} else {
-			// No name: take the id as the name
-			params.material->name = parser->ToString(params.id);
-			params.material->has_name = false;
+		if(params.material != nullptr) {
+			if(params.name_opt) {
+				params.material->name = *(params.name_opt);
+				params.material->has_name = true;
+			} else {
+				// No name: take the id as the name
+				params.material->name = parser->ToString(params.id);
+				params.material->has_name = false;
+			}
 		}
 	}
 
