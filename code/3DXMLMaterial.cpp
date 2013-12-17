64e074837a4c44de8a1e2f63a91d8f7424b604cc
Léo Terziman <leo.terziman@clarte.asso.fr>
2013-12-17 11:28:30 +0100

    In 3DXML: updated log message to avoid garbaging the logs

--- code/3DXMLMaterial.cpp
+++ code/3DXMLMaterial.cpp
@@ -453,7 +453,8 @@ namespace Assimp {
 				if(params.mapping_operator != aiTextureMapping_UV) {
 					mMaterial->AddProperty((int*) &params.mapping_operator, 1, AI_MATKEY_MAPPING_DIFFUSE(0));
 				} else {
-					DefaultLogger::get()->warn("In Feature \"" + mReader.ToString(id) + "\": Operator mapping defined but no operator provided.");
+					// No explicit id for this warning message as most 3DXML exporter generates invalid materials and repeated output garbage the logs
+					DefaultLogger::get()->warn("In Feature: Operator mapping defined but no operator provided.");
 				}
 				break;
 			case ENVIRONMENT_MAPPING:
